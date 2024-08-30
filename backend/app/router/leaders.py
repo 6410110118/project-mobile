cdf26a251583d8d527a36259c042561dd2028e73
6410110118 <6410110118@psu.ac.th>
2024-08-30 19:56:11 +0700

    add api update for leader

--- backend/app/router/leaders.py
+++ backend/app/router/leaders.py
@@ -16,7 +16,7 @@ from ..models import (
     get_session,
 )
 
-router = APIRouter(prefix="/leaders")
+router = APIRouter(prefix="/leaders" , tags=["leaders"])
 
 
 from .. import deps
@@ -49,12 +49,41 @@ async def update_leader(
     session: Annotated[AsyncSession, Depends(get_session)],
     current_user: User = Depends(deps.get_current_user),
 ) -> Leader:
-    data = leader.dict()
-    db_leader = await session.get(DBLeader, leader_id)
-    db_leader.sqlmodel_update(data)
-    session.add(db_leader)
-    await session.commit()
-    await session.refresh(db_leader)
+    if current_user.role != "Leader":
+        raise HTTPException(status_code=403, detail="Only leader can update leader")
+    result = await session.exec(
+        select(DBLeader).where(DBLeader.id == leader_id)
+    )
+    db_leader = result.one_or_none()
+    
+    if  db_leader :
+        db_leader.sqlmodel_update(leader)
+        await session.commit()
+        session.add(db_leader)
+        await session.commit()
+        await session.refresh(db_leader)
 
-    return Leader.from_orm(db_leader)
+        return Leader.from_orm(db_leader)
+    raise HTTPException(status_code=404, detail="Leader not found")
+
+# @router.delete("/{leader_id}")
+
+# async def delete_leader(
+#     leader_id: int,
+#     session: Annotated[AsyncSession, Depends(get_session)],
+#     current_user: User = Depends(deps.get_current_user),
+#     ) -> dict:
+#     if  current_user.role  != "Leader":
+#         raise HTTPException(status_code=403, detail="Only leader can delete leader")
+#     result = await session.exec(
+#         select(DBLeader).where(DBLeader.id == leader_id)
+
+#     )
+#     db_leader = result.one_or_none()
+    
+#     if db_leader :
+#         await session.delete(db_leader)
+#         await session.commit()
+#         return {"message": "Leader deleted successfully"}
+#     raise HTTPException(status_code=404, detail="Leader not found")
 
