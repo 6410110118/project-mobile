87b0584d7353d4c45b6b02a550162970461709bb
BigDev <sukree5284@gmail.com>
2024-09-07 12:53:02 +0700

    fix to homepage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/components/login.dart';
+import 'package:frontend/components/home.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +12,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage(),
+      home: HomePage(),
+      routes: {
+        
+      },
     );
   }
 }
