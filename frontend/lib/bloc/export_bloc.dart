93a74e7e18ea8a1734471c612649710d5606c48a
6410110118 <6410110118@psu.ac.th>
2024-09-30 15:51:10 +0700

    fix here

--- frontend/lib/bloc/export_bloc.dart
+++ frontend/lib/bloc/export_bloc.dart
@@ -1,3 +1,11 @@
-export 'bloc.dart';
-export 'bloc_event.dart';
-export 'bloc_state.dart';
+export 'trip/bloc.dart';
+export 'trip/bloc_event.dart';
+export 'trip/bloc_state.dart';
+export 'navigation/bloc.dart';
+export 'navigation/bloc_event.dart';
+export 'navigation/bloc_state.dart';
+export 'login/login_bloc.dart';
+
+export 'login/login_event.dart';
+
+export 'login/login_state.dart';
