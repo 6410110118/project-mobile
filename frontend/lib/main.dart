3e872c9a6d53c47abba4dbef5ec208d416462ce4
6410110118 <6410110118@psu.ac.th>
2024-10-02 17:44:29 +0700

    add register to main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/bloc/welcome/welcome_event.dart';
+import 'package:frontend/repositories/user_repository.dart'; // อย่าลืม import UserRepository
 import 'package:frontend/screen/change_password.dart';
 import 'package:frontend/screen/login.dart';
 import 'package:frontend/screen/register_page.dart';
+import 'package:frontend/screen/sign_in_page.dart';
 import 'package:frontend/screen/welcome_screens.dart';
+import 'package:provider/provider.dart';
 import 'bloc/export_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider(
-        create: (_) => TravelBloc()..add(LoadTravelPage()),
-        child: TravelPage(),
+    return MultiProvider(
+      providers: [
+        Provider<UserRepository>(create: (_) => UserRepository()), // ให้บริการ UserRepository
+        BlocProvider<TravelBloc>(create: (_) => TravelBloc()..add(LoadTravelPage())), // ให้บริการ TravelBloc
+        // คุณสามารถเพิ่ม Bloc อื่น ๆ ที่นี่ถ้าจำเป็น
+      ],
+      child: MaterialApp(
+        title: 'Plan For Travel',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const TravelPage(),
+        routes: {
+          '/signin': (context) =>  SignInPage(),
+          '/login': (context) => const Login(),
+          '/register': (context) =>  RegisterPage(),
+          '/changepassword': (context) => const ChangePasswordPage(),
+        },
       ),
-      routes: {
-        '/signin': (context) => SignInPage(),
-        '/login': (context) => const Login(),
-        '/register': (context) => const RegisterPage(),
-        '/changepassword': (context) => const ChangePasswordPage(),
-      },
     );
   }
-}
-
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Plan For Travel',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: TravelPage(), // ไม่ส่ง trips เข้าไป
-  );
 }
