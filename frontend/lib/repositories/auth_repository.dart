646ef1bd710386202b2a5ada55c55a8b3959c414
6410110118 <6410110118@psu.ac.th>
2024-10-02 17:46:01 +0700

    add refrash token

--- frontend/lib/repositories/auth_repository.dart
+++ frontend/lib/repositories/auth_repository.dart
@@ -22,9 +22,13 @@ class AuthRepository {
       );
 
       if (response.statusCode == 200) {
-        final token = response.data['access_token']; // ตรวจสอบคีย์ที่ถูกต้อง
-        await tokenStorage.saveToken(token);
-        return token;
+        final accessToken = response.data['access_token'];
+        final refreshToken = response.data['refresh_token'];
+
+        // บันทึกทั้ง access token และ refresh token
+        await tokenStorage.saveToken(accessToken);
+        await tokenStorage.saveRefreshToken(refreshToken);
+        return accessToken;
       } else {
         throw Exception('Failed to login: ${response.data}');
       }
@@ -33,8 +37,36 @@ class AuthRepository {
     }
   }
 
+  Future<String> refreshToken() async {
+    try {
+      final refreshToken = await tokenStorage.getRefreshToken();
+      final response = await dio.post(
+        '/token/refresh',
+        options: Options(
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded', // กำหนด Content-Type
+          },
+        ),
+        data: {
+          'refresh_token': refreshToken,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final newAccessToken = response.data['access_token'];
+        await tokenStorage.saveToken(newAccessToken);
+        return newAccessToken;
+      } else {
+        throw Exception('Failed to refresh token: ${response.data}');
+      }
+    } catch (e) {
+      throw Exception('Failed to refresh token: $e');
+    }
+  }
+
   Future<void> logout() async {
     await tokenStorage.deleteToken();
+    await tokenStorage.deleteRefreshToken(); // ลบ refresh token ด้วย
     // Add any additional logout logic here
   }
 }
