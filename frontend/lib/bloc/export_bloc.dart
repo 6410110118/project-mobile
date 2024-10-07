09cba2e818a8c886ddf9643e61a57a1e93875978
6410110118 <6410110118@psu.ac.th>
2024-10-07 20:47:35 +0700

    add profile bloc

--- frontend/lib/bloc/export_bloc.dart
+++ frontend/lib/bloc/export_bloc.dart
@@ -1,22 +1,31 @@
 export 'trip/bloc.dart';
 export 'trip/bloc_event.dart';
 export 'trip/bloc_state.dart';
+
 export 'navigation/bloc.dart';
 export 'navigation/bloc_event.dart';
 export 'navigation/bloc_state.dart';
-export 'login/login_bloc.dart';
+
+
 export 'changepassword/change_password_event.dart';
 export 'changepassword/change_password_bloc.dart';
 export 'changepassword/change_password_state.dart';
+
 export 'register/register_bloc.dart';
 export 'register/register_event.dart';
 export 'register/register_state.dart';
+
 export 'welcome/welcome_state.dart';
 export 'welcome/welcome_bloc.dart';
+
 export 'group/group_bloc.dart';
 export 'group/group_event.dart';
 export 'group/group_state.dart';
 
 export 'login/login_event.dart';
-
+export 'login/login_bloc.dart';
 export 'login/login_state.dart';
+
+export 'user/user_bloc.dart';
+export 'user/user_event.dart';
+export 'user/user_state.dart';
