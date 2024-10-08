4aeb72d5400184218d2fde966de3b6b1c98a1bae
6410110260 <125025570+6410110260@users.noreply.github.com>
2024-10-08 19:50:42 +0700

    add group models

--- frontend/lib/models/groups.dart
+++ frontend/lib/models/groups.dart
@@ -1,13 +1,22 @@
 class Group {
   final String name;
+  final DateTime startDate;
+  final DateTime endDate;
+  final String? imageUrl; // Nullable เนื่องจากอาจไม่มีรูป
 
   Group({
     required this.name,
+    required this.startDate,
+    required this.endDate,
+    this.imageUrl,
   });
 
   factory Group.fromJson(Map<String, dynamic> json) {
     return Group(
-      name: json['name'],
+      name: json['name'] ?? 'Unknown Name',
+      startDate: DateTime.parse(json['start_date']),
+      endDate: DateTime.parse(json['end_date']),
+      imageUrl: json['image_url'],
     );
   }
 }
