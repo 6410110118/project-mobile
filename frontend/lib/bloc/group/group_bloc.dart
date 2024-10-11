7cc5c7d414201d575077c5e7b1b4ddd8dfcb1222
6410110260 <125025570+6410110260@users.noreply.github.com>
2024-10-11 20:03:01 +0700

    fix group bloc

--- frontend/lib/bloc/group/group_bloc.dart
+++ frontend/lib/bloc/group/group_bloc.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/repositories/group_repository.dart';
-
 import '../export_bloc.dart';
 
-class GroupBloc extends Bloc<FetchGroupEvent, GroupState> {
+class GroupBloc extends Bloc<GroupEvent, GroupState> {
   final GroupRepository groupRepository;
 
   GroupBloc({required this.groupRepository}) : super(GroupStateInitial()) {
@@ -16,6 +14,28 @@ class GroupBloc extends Bloc<FetchGroupEvent, GroupState> {
         emit(GroupStateLoaded(groupList));
       } catch (error) {
         emit(GroupError('Failed to load data. Error: $error'));
+        print(error);
+      }
+    });
+
+    // Handle CreateGroupEvent
+    on<CreateGroupEvent>((event, emit) async {
+      emit(GroupStateLoading());
+
+      try {
+        final newGroup = await groupRepository.createGroup(
+          name: event.newGroup.name,
+          startDate: event.newGroup.startDate,
+          endDate: event.newGroup.endDate,
+        );
+        emit(GroupCreated('Group created successfully: ${newGroup.name}'));
+
+        // เพิ่ม await ตรงนี้เพื่อให้มั่นใจว่า FetchGroupEvent ทำงานหลังจากกลุ่มถูกสร้างแล้ว
+        await Future.delayed(
+            Duration(milliseconds: 500)); // Optional: delay เพื่อให้มั่นใจ
+        add(FetchGroupEvent());
+      } catch (error) {
+        emit(GroupError('Failed to create group. Error: $error'));
         print(error);
       }
     });
