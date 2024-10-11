0415677b10bf866ae30544f8fe49e07b82dc283d
6410110260 <125025570+6410110260@users.noreply.github.com>
2024-10-11 20:03:10 +0700

    fix group event

--- frontend/lib/bloc/group/group_event.dart
+++ frontend/lib/bloc/group/group_event.dart
@@ -1,6 +1,16 @@
+import 'package:frontend/models/groups.dart';
+
 abstract class GroupEvent {}
 
 class FetchGroupEvent extends GroupEvent {
   
   FetchGroupEvent();
 }
+// Event สำหรับสร้างกลุ่ม
+class CreateGroupEvent extends GroupEvent {
+  final Group newGroup;
+
+  CreateGroupEvent({required this.newGroup});
+}
+
+
