2573b69d242073911c8c2674eb0ce162b2d8d69f
6410110260 <125025570+6410110260@users.noreply.github.com>
2024-10-14 17:00:07 +0700

    fix trip models

--- frontend/lib/models/trip.dart
+++ frontend/lib/models/trip.dart
@@ -9,6 +9,7 @@ class Trip {
   bool? role;
   bool? isDone;
   DateTime? doneTime;
+  bool? isApproved; // Add this property
 
   // Constructor
   Trip({
@@ -22,6 +23,7 @@ class Trip {
     this.role = false,
     this.isDone = false,
     this.doneTime,
+    this.isApproved = false, // Default value
   });
 
   // Factory constructor to create a Trip instance from JSON
@@ -30,13 +32,17 @@ class Trip {
         tripName: json['name'],
         description: json['description'],
         address: json['address'],
-        
         imageUrl: json['photo_reference'],
-        starttime: json['start_date'] != null ? DateTime.parse(json['start_date']) : null,
-        endtime: json['end_date'] != null ? DateTime.parse(json['end_date']) : null,
-        role: json['role'] == 'Leader', // สมมติว่า role เป็น string "Leader" หรืออย่างอื่น
+        starttime: json['start_date'] != null
+            ? DateTime.parse(json['start_date'])
+            : null,
+        endtime:
+            json['end_date'] != null ? DateTime.parse(json['end_date']) : null,
+        role: json['role'] == 'Leader',
         isDone: json['isDone'] ?? false,
-        doneTime: json['doneTime'] != null ? DateTime.parse(json['doneTime']) : null,
+        doneTime:
+            json['doneTime'] != null ? DateTime.parse(json['doneTime']) : null,
+        isApproved: json['isApproved'] ?? false, // Parse isApproved from JSON
       );
 
   // Method to convert a Trip instance into JSON
@@ -51,13 +57,13 @@ class Trip {
         'role': role,
         'isDone': isDone,
         'doneTime': doneTime?.toIso8601String(),
+        'isApproved': isApproved, // Add isApproved to JSON output
       };
-
-
 
   // Getter to check if the trip is late
   bool get isLate => endtime != null && DateTime.now().isAfter(endtime!);
 
   // Getter to check if the trip is upcoming
-  bool get isUpcoming => starttime != null && DateTime.now().isBefore(starttime!);
+  bool get isUpcoming =>
+      starttime != null && DateTime.now().isBefore(starttime!);
 }
